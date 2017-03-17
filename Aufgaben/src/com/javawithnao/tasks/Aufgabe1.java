b0624401d64f3f939cf80b3fb1b6e1d4e05a29b7
Tarek Chebbi <tc461294@win.zlw-ima.rwth-aachen.de>
2017-03-17 20:01:34 +0100

    Signed-off-by: Tarek Chebbi <tc461294@win.zlw-ima.rwth-aachen.de>

---
+++ Aufgaben/src/com/javawithnao/tasks/Aufgabe1.java
@@ -0,0 +1,22 @@
+package com.javawithnao.tasks;
+
+/**
+ * Primitive Datentypen und Variablen:
+ * NAO Rechnet
+ */
+public class Aufgabe1 {
+
+    public static void main(String[] args) {
+        //Hier könnt ihr eure Deklarationen und Initialisierungen schreiben:
+
+        //System.out.println(...) gibt eure Ergebnisse in der Konsole aus, wenn ihr auf den Play-Button drückt.
+        //Wie genau das funktioniert seht ihr später!
+        //Dass die Variablen rot angezeigt werden, liegt daran dass ihr sie noch nicht Deklariert habt :)
+        System.out.println(x + "+" + y + "=" + summe);
+        if (x - y == differenz) System.out.println(x + "-" + y + "=" + differenz);
+        else System.out.println(y + "-" + x + "=" + differenz);
+        System.out.println(x + "*" + y + "=" + produkt);
+        if (x / y == quotient) System.out.println(x + "/" + y + "=" + quotient);
+        else System.out.println(y + "/" + x + "=" + quotient);
+    }
+}
---
+++ Aufgaben/src/com/javawithnao/tasks/Aufgabe2.java
@@ -0,0 +1,15 @@
+package com.javawithnao.tasks;
+
+/**
+ * Primitive Datentypen und Variablen:
+ * NAO Rechnet
+ */
+public class Aufgabe2 {
+
+    public static void main(String[] args) {
+        double a = 0.2;
+        double b = 0.8;
+        double differenz = a + b;
+    }
+
+}
