a832344cfff97fb8defa6bf05196f1e61a994969
Martin Schwarzbach <ms617455@win.zlw-ima.rwth-aachen.de>
2018-03-28 08:40:32 +0200

    fixed batch file

--- Entwicklung/Mini-Programme/JavaPaths.bat
+++ Entwicklung/Mini-Programme/JavaPaths.bat
@@ -1,13 +1,20 @@
 @ECHO OFF
 CALL :testLabelExist "%COMPUTERNAME%" && GOTO %COMPUTERNAME% || GOTO INVALID
 
+:EDULAB-10
+SET javac="C:\Program Files (x86)\Java\jdk1.8.0_131\bin\javac.exe"
+SET java="C:\Program Files (x86)\Java\jdk1.8.0_131\bin\java.exe"
+EXIT /B
+
 :EDULAB-20
 SET javac="C:\Program Files (x86)\Java\jdk1.8.0_144\bin\javac.exe"
 SET java="C:\Program Files (x86)\Java\jdk1.8.0_144\bin\java.exe"
+EXIT /B
 
 :TAREK-WINX
 SET javac="C:\Program Files\Java\jdk1.8.0_91\bin\javac.exe"
 SET java="C:\Program Files\Java\jdk1.8.0_91\bin\java.exe"
+EXIT /B
 
 :testLabelExist
 FOR /f "delims=" %%t IN (
